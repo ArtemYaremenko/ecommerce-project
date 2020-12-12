443daea575364778455a3fb64a06f2a8f88992ec
Artem <Artem.Yaremenko.1992@gmail.com>
2020-12-13 00:09:17 +0300

    Add user registration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,5 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
